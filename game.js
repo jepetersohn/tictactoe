0f3d0e4d0eaffecb8dd10e603e2742d46dbba6f3
alanahanson <jepetersohn@gmail.com>
2017-04-09 15:05:20 -0500

    working on logic for finding a winner. also preventing cells from being clicked more than once

--- game.js
+++ game.js
@@ -1,20 +1,32 @@
 $(document).ready(function() {
   handleClick();
+  updateCell();
+  winner();
 });
 
 var handleClick = function() {
   var clickedCell;
   var i = 0;
   $("td").on("click", function(event) {
-    if (i === 0){
     clickedCell = $(this);
-    updateCell(clickedCell, "playerX");
-    i++;
-  } else {
-    clickedCell = $(this);
-    updateCell(clickedCell, "playerO");
+    if ($(this).closest('td').hasClass("clicked")) {
+        alert("Pick a different space.")
+      }
+    else if (i === 0){
+      updateCell(clickedCell, "playerX");
+      clickedCell.addClass("clicked X");
+        if ((clickedCell).closest("tr").find(".X").size() === 3) {
+          alert("winner!")};
+      i++;
+    } else {
+      updateCell(clickedCell, "playerO");
+      clickedCell.addClass("clicked O");
     i = 0;
   }
+
+    if (winner()) {
+      alert("game over");
+    }
   });
 };
 
@@ -25,3 +37,11 @@ var updateCell = function(cell, player) {
     cell.closest('td').text("O");
   }
 };
+
+var winner = function() {
+  return checkRow();
+}
+
+var checkRow = function() {
+  return false;
+}
